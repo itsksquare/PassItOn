334e3d63be5e96cba45bc3c3e9bdbc4a324a2ba6
Kartikey Khanna <kartikeykhanna20@gmail.com>
2023-07-02 16:06:02 +0530

    Images added

---
+++ public/images/index.js
@@ -0,0 +1,27 @@
+import donate from "./donate.png";
+import donateimage from "./donateimage.png";
+import laststep from "./laststep.png";
+import sitelogo from "./logo.png";
+import select from "./select.png";
+import acc from "./acc.png";
+import books from "./books.png";
+import clothes from "./clothes.png";
+import electronics from "./electronics.png";
+import buybanner from "./buybanner.png";
+import footwear from "./footwear.png";
+import furniture from "./furniture.png";
+
+export {
+  donate,
+  donateimage,
+  laststep,
+  sitelogo,
+  select,
+  acc,
+  books,
+  clothes,
+  electronics,
+  buybanner,
+  footwear,
+  furniture,
+};
