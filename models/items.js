b3b6b8bb10d129adee79ef6a0bf39945bc3f4c08
Kartikey Khanna <kartikeykhanna20@gmail.com>
2023-07-02 13:47:13 +0530

    Shadcn ui added

---
+++ models/items.js
@@ -0,0 +1,31 @@
+import { Schema, model, models } from "mongoose";
+
+const ItemSchema = new Schema({
+  donator: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  donation_type: {
+    type: String,
+    required: [true, "Type is required!"],
+  },
+  item_category: {
+    type: String,
+    required: [true, "Category is required!"],
+  },
+  item_title: {
+    type: String,
+    required: [true, "Title is required!"],
+  },
+  item_description: {
+    type: String,
+    required: [true, "Description is required!"],
+  },
+  item_address: {
+    type: String,
+    required: [true, "Address is required!"],
+  },
+});
+
+const Item = models.Item || model("Item", ItemSchema);
+export default Item;
